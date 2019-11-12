f7e6a924374c65f7718417603ab994f0793157de
Leo-ADAM <leo.adam045@gmail.com>
2019-11-12 16:34:46 +0100

    kata string

--- src/kata/StringCompare.java
+++ src/kata/StringCompare.java
@@ -1,4 +1,17 @@
 public class StringCompare {
 
     // TODO
+	public static int compare(String word1, String word2) {
+		if(word1 == null || word2 == null || word1.length() != word2.length()) {
+			return -1;
+		}
+		int nb = 0;
+		for (int i=0; i < word1.length(); i++) {
+			if (word1.charAt(i) == word2.charAt(i)) {
+				nb++;
+			}
+		}
+		return nb;
+	
+	}
 }
--- src/test/StringCompareTest.java
+++ src/test/StringCompareTest.java
@@ -7,6 +7,41 @@ public class StringCompareTest {
     public void testTrue() throws Exception {
         Assert.assertEquals(true, true);
     }
-
     // TODO
+    @Test
+    public void test1() throws Exception {
+    	Assert.assertEquals(1, StringCompare.compare("a", "a"));
+    }
+    @Test
+    public void test2() throws Exception {
+    	Assert.assertEquals(0, StringCompare.compare("a", "b"));
+    }
+    @Test
+    public void test3() throws Exception {
+    	Assert.assertEquals(1, StringCompare.compare("aa", "ba"));
+    }
+    @Test
+    public void test4() throws Exception {
+    	Assert.assertEquals(3, StringCompare.compare("cassis", "castor"));
+    }
+    @Test
+    public void test5() throws Exception {
+    	Assert.assertEquals(-1, StringCompare.compare("tacos", "poulpe"));
+    }
+    @Test
+    public void test6() throws Exception {
+    	Assert.assertEquals(-1, StringCompare.compare(null, null));
+    }
+    @Test
+    public void test7() throws Exception {
+    	Assert.assertEquals(-1, StringCompare.compare(null, "toto"));
+    }
+    @Test
+    public void test8() throws Exception {
+    	Assert.assertEquals(-1, StringCompare.compare("titi", null));
+    }
+    @Test
+    public void test9() throws Exception {
+    	Assert.assertEquals(0,StringCompare.compare("", ""));
+    }
 }
