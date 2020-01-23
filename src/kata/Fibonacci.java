afc1058337f4792b3253d93cf3772a2f0a3b29a1
Leo-ADAM <leo.adam045@gmail.com>
2020-01-23 11:52:52 +0100

    quete done

--- src/kata/Fibonacci.java
+++ src/kata/Fibonacci.java
@@ -1,4 +1,11 @@
 public class Fibonacci {
 
-    // TODO
+	public static int fibo(int n) {
+
+		if (n <= 1) {
+			return n;
+		} else {
+			return fibo(n - 1) + fibo(n - 2);
+		}
+	}
 }
--- src/test/FibonacciTest.java
+++ src/test/FibonacciTest.java
@@ -8,5 +8,11 @@ public class FibonacciTest {
         Assert.assertEquals(true, true);
     }
 
-    // TODO
+    @Test
+    public void test1() throws Exception {
+        Assert.assertEquals(0, Fibonacci.fibo(0));
+        Assert.assertEquals(1, Fibonacci.fibo(1));
+        Assert.assertEquals(3, Fibonacci.fibo(4));
+        Assert.assertEquals(5, Fibonacci.fibo(5));
+    }
 }
